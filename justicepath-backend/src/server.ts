c86f8e91c6a3c3551d1c13ca149ad4ed0f47ae01
farmerj50 <gabby1905@live.com>
2025-08-08 19:41:04 -0400

    updtae port for railway

--- justicepath-backend/src/server.ts
+++ justicepath-backend/src/server.ts
@@ -3,10 +3,8 @@ import app from './app';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PORT = Number(process.env.PORT) || 5000;
 
-const PORT = process.env.PORT || 5000;
-
-
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
